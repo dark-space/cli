4fec96f2060bede4ff419b49c53df495eded29e8
dark-space <moc.7iamg@gmail.com>
2018-08-15 17:47:32 +0900

    add option to delete tail newlines

--- shownl.nim
+++ shownl.nim
@@ -1,4 +1,4 @@
-import strutils, parseopt
+import strutils, parseopt, nre
 import lib/io
 
 var args: seq[string] = @[]
@@ -6,16 +6,20 @@ var args: seq[string] = @[]
 proc usage() =
   let s = """
 Usage: newlines [OPTION]... [FILE]
-  -s=<str>: Specify substituted str."""
+  -s=<str>: Specify substituted str.
+  -t      : Delete tail newlines."""
   echo s
 
 var subst = "\\n"
+var delete_tail = false
 try:
   for kind, key, val in getopt():
     if kind == cmdArgument:
       args.add(key)
     if kind == cmdShortOption and key == "s":
       subst = val
+    if kind == cmdShortOption and key == "t":
+      delete_tail = true
     else:
       usage()
       quit(0)
@@ -23,5 +27,8 @@ except:
   usage()
   quit(1)
 
-stdout.write(readRaw(args).replace("\r","\\r").replace("\n", subst))
+var str = readRaw(args)
+if delete_tail:
+  str = str.replace(re"\s+$","")
+stdout.write(str.replace("\r","\\r").replace("\n", subst))
 
