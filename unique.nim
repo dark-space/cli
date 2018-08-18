3e8909919a1ca2e3688c8788910a3cfa9efd30f6
dark-space <moc.7iamg@gmail.com>
2018-08-18 15:40:13 +0900

    backward serach

--- unique.nim
+++ unique.nim
@@ -1,18 +1,22 @@
-import strutils, sets, tables, parseopt, nre
+import strutils, sets, tables, parseopt, nre, algorithm
 import lib/io
 
 var args: seq[string] = @[]
-var opts = initTable[string,string]()
 
 proc usage() =
   let s = """
-Usage: unique [FILE]"""
+Usage: line [OPTION]... [FILE]
+  -b      : backward search."""
   echo s
+
+var forward = true
 
 try:
   for kind, key, val in getopt():
     if kind == cmdArgument:
       args.add(key)
+    elif kind == cmdShortOption and key == "b":
+      forward = false
     else:
       usage()
       quit(0)
@@ -23,8 +27,17 @@ except:
 
 var already = initSet[string]()
 let lines = read(args).split("\n")
-for line in lines:
-  if not already.contains(line):
+if forward:
+  for line in lines:
+    if not already.contains(line):
+      echo line
+      already.incl(line)
+else:
+  var outLines: seq[string] = @[]
+  for line in lines.reversed:
+    if not already.contains(line):
+      outLines.add(line)
+      already.incl(line)
+  for line in outLines.reversed:
     echo line
-    already.incl(line)
 
