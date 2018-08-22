af3ead09c9184844d4f012b015c7009a3a14b775
dark-space <moc.7iamg@gmail.com>
2018-08-22 17:25:41 +0900

    bin.20180822.tar.gz

--- download-bin.sh
+++ download-bin.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/dark-space/cli/master/bin.20180815.tar.gz
-tar xvf bin.20180815.tar.gz
-rm -f bin.20180815.tar.gz
+wget https://raw.githubusercontent.com/dark-space/cli/master/bin.20180822.tar.gz
+tar xvf bin.20180822.tar.gz
+rm -f bin.20180822.tar.gz
