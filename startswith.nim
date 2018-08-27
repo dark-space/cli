b7732832468ebd2528d24def4aed04feb5b0c5fa
dark-space <moc.7iamg@gmail.com>
2018-08-27 15:26:00 +0900

    add ignore_case option

--- startswith.nim
+++ startswith.nim
@@ -4,10 +4,12 @@ import lib/io
 proc usage() =
   let s = """
 Usage: startswith [OPTION]... QUERY [FILE]
+  -i      : ignore case.
   -m      : remove coloring.
   -s      : remove leading spaces."""
   echo s
 
+var ignore_case = false
 var monochrome = false
 var remove_space = false
 
@@ -16,6 +18,8 @@ try:
   for kind, key, val in getopt():
     if kind == cmdArgument:
       args.add(key)
+    elif (kind == cmdShortOption and key == "i"):
+      ignore_case = true
     elif (kind == cmdShortOption and key == "m"):
       monochrome = true
     elif (kind == cmdShortOption and key == "s"):
@@ -27,10 +31,14 @@ except:
   usage()
   quit(1)
 
-let query = args[0]; args.delete(0)
+var query = args[0]; args.delete(0)
+if ignore_case:
+  query = query.toLower
 let lines = read(args).split("\n")
 for orig_line in lines:
   var l = orig_line
+  if ignore_case:
+    l = l.toLower
   if monochrome:
     l = l.replace(re"\[\d+m","")
   if remove_space:
